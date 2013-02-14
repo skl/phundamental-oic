9271eb4925898142b22f1d537506c4bf5180b1bf
Stephen Lang <stephen.lang@symphony-group.co.uk>
2013-02-14 13:52:19 +0000

    Implemented Mac installation

--- install.sh
+++ install.sh
@@ -24,6 +24,32 @@ case "${PH_OS}" in \
         done
     ;;
 
+    "mac")
+        # 64bit instantclient seg faults on OSX, force 32bit
+        for i in `ls -1 ${PH_OIC_INSTALL_DIR}/${PH_OS}/32bit/*.zip`; do
+            unzip ${i} -d /tmp/phundamental-oic
+        done
+
+        LIBDIR="/usr/local/instantclient"
+        ph_mkdirs ${LIBDIR}
+
+        mv /tmp/phundamental-oic/instantclient_10_2/* ${LIBDIR}
+        rm -rf /tmp/phundamental-oic
+
+        ph_symlink ${LIBDIR}/sqlplus /usr/local/bin/sqlplus true
+
+        # Export environmental variables now so that PHP installation works right away
+        export ORACLE_HOME=${LIBDIR}
+        export LYLD_LIBRARY_PATH=${LIBDIR}
+
+        echo "You need to add the following lines to your ~/.profile:"
+        echo "export ORACLE_HOME=${LIBDIR}"
+        echo "export LYLD_LIBRARY_PATH=${LIBDIR}"
+        echo ""
+
+        read -p "Press any key to continue..."
+    ;;
+
     *)
         echo "Instant Client installtion not implemented for this OS yet!"
 esac
