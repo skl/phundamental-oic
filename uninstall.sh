d782df082f66c112107baec0c05939e2f3476ce3
Stephen Lang <stephen.lang@symphony-group.co.uk>
2014-04-22 12:33:43 +0100

    [oic] Add Debian support

--- uninstall.sh
+++ uninstall.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
-case "${PH_OS}" in \
-    "linux")
-        for i in `rpm -qa | grep -i oracle`; do
-            rpm -e ${i}
-        done
+case "${PH_OS}" in
+"linux")
+    case "${PH_OS_FLAVOUR}" in
+    "debian")
+        PH_OIC_UNINSTALL_CMD='dpkg -r'
+        PH_OIC_UNINSTALL_SEARCH='dpkg -l'
+        ;;
 
-        test ${PH_ARCH} == '32bit' && LIBDIR='client' || LIBDIR='client64'
-        LIBDIR="/usr/lib/oracle/10.2.0.5/${LIBDIR}/lib"
-
-        # Remove library path if exists
-        ph_search_and_replace "${LIBDIR}" "" /etc/ld.so.conf
-        ldconfig
-
-        echo ""
-        echo "Uninstalled all RPMs matching 'oracle'."
-    ;;
+    "suse")
+        PH_OIC_UNINSTALL_CMD='rpm -e'
+        PH_OIC_UNINSTALL_SEARCH='rpm -qa'
+        ;;
 
     *)
-        echo "Instant Client uninstallation not implemented for this OS yet!"
+        echo "Instant Client installtion not implemented for this OS yet!"
+        return 1 || exit 1
+        ;;
+    esac
+
+    echo "You should run the following commands at your own risk:"
+    for i in `${PH_OIC_UNINSTALL_SEARCH} | grep -i oracle`; do
+        echo ${PH_OIC_UNINSTALL_CMD} ${i}
+    done
+
+    test ${PH_ARCH} == '32bit' && LIBDIR='client' || LIBDIR='client64'
+    LIBDIR="/usr/lib/oracle/10.2.0.5/${LIBDIR}/lib"
+
+    # Remove library path if exists
+    ph_search_and_replace "${LIBDIR}" "" /etc/ld.so.conf
+    ldconfig
+    ;;
+
+*)
+    echo "Instant Client uninstallation not implemented for this OS yet!"
+    return 1 || exit 1
+    ;;
 esac
 
